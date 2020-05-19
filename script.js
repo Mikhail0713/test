e4144ab3b2ba9da5acc38084566fde25a295d4c8
Mike <4ex@gmail.com>
2020-05-19 12:35:05 +0400

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+let arr = 56;
+
+console.log(arr);
+
+let answer = prompt("A u ok?", "fuck u");
+
+console.log(+answer);
