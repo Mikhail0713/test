74adafe7b3b5fd8f4cc66af7c972a2430e6288ed
Mike <4ex@gmail.com>
2020-06-04 12:46:46 +0400

    04.06

--- script.js
+++ script.js
@@ -1,41 +1,116 @@
-let box = document.getElementById('box'),
-    btn = document.getElementsByTagName('button'),
-    circle = document.getElementsByClassName('circle'),
-    heart = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('.heart'),
-    wrapper = document.querySelector('.wrapper');
+// let box = document.getElementById('box'),
+//     btn = document.getElementsByTagName('button'),
+//     circle = document.getElementsByClassName('circle'),
+//     heart = document.querySelectorAll('.heart'),
+//     oneHeart = document.querySelector('.heart'),
+//     wrapper = document.querySelector('.wrapper');
 
-box.style.backgroundColor = 'blue';
-btn[1].style.borderRadius = '100%';
+// box.style.backgroundColor = 'blue';
+// btn[1].style.borderRadius = '100%';
 
-circle[0].style.backgroundColor = 'red';
-circle[1].style.backgroundColor = 'yellow';
-circle[2].style.backgroundColor = 'green';
+// circle[0].style.backgroundColor = 'red';
+// circle[1].style.backgroundColor = 'yellow';
+// circle[2].style.backgroundColor = 'green';
 
-// for (let i=0; i < heart.length; i++) {
-//     heart[i].style.backgroundColor = "blue";
-// }
+// // for (let i=0; i < heart.length; i++) {
+// //     heart[i].style.backgroundColor = "blue";
+// // }
 
-// heart.forEach(function(item,i, hearts) {
-//     item.style.backgroundColor = 'blue';
+// // heart.forEach(function(item,i, hearts) {
+// //     item.style.backgroundColor = 'blue';
+// // });
+
+// let div = document.createElement('div'),
+//     text = document.createTextNode('Тут был я');
+
+// div.classList.add('black');
+
+// // wrapper.appendChild(div);
+
+// // div.innerHTML = '<h1>Hello World!</h1>';
+// div.textContent = 'Hellow world!';
+
+// document.body.insertBefore(div, circle[0]);
+// document.body.removeChild(circle[1]);
+// wrapper.removeChild(heart[1]);
+// document.body.replaceChild(btn[1], circle[2]);
+// console.log(div);
+
+
+let btn = document.querySelectorAll('button'),
+    wrap = document.querySelector(".wrapper"),
+    link = document.querySelector('a');
+
+// btn[0].onclick = function() {
+//     alert('Ты че на кнопку нажал, попух???')
+// };
+
+// btn[0].onclick = function() {
+//     alert('Опять нажал, охуел что ли?')
+// };
+
+// btn[0].addEventListener('click',function(event){
+//     // console.log(event);
+//     // let target = event.target;
+//     // target.style.display = 'none';
+
+//     console.log('Произошло событие: ' +  event.type + 'На элементе' + event.target);
+    
 // });
 
-let div = document.createElement('div'),
-    text = document.createTextNode('Тут был я');
+// wrap.addEventListener('click', function(){
+//     console.log('Произошло событие: ' +  event.type + 'На элементе' + event.target);
+// });
 
-div.classList.add('black');
+// link.addEventListener('click', function(event){
+//     event.preventDefault();
+//     console.log('Произошло событие: ' +  event.type + 'На элементе' + event.target);
+// });
 
-// wrapper.appendChild(div);
+// btn.forEach(function(item){
+//     item.addEventListener('mouseleave', function(){
+//         console.log("У вас мышка убежала!");
+//     });
+// });
+// // btn[0].addEventListener('mouseenter', function() {
+// //     alert('Ты чего на кнопку мышь навел?');
+// // }); 
 
-// div.innerHTML = '<h1>Hello World!</h1>';
-div.textContent = 'Hellow world!';
+//touchstart
+//touchmove
+//touchend
 
-document.body.insertBefore(div, circle[0]);
-document.body.removeChild(circle[1]);
-wrapper.removeChild(heart[1]);
-document.body.replaceChild(btn[1], circle[2]);
+//touchenter
+//touchleave
+//touchcancel
+
+    // new RegExp('pattern', 'flags');
+    // /pattern/
+
+    // let ans = prompt('Введите ваше имя');
+
+    // let reg = /n/gi;
+
+    // // console.log(ans.search(reg));
+    // // console.log(ans.match(reg));
+    // console.log(reg.test(ans));
 
 
+    // let pass = prompt('Введите пароль');
 
+    // console.log(pass.replace(/./g,'*'));
+    // alert('12-34-56'.replace(/-/g, ':'));
 
-console.log(div);
+//  \d \D
+//  \w \W
+//  \s \S
+
+// let ans = prompt('Введите число');
+
+//     let reg = /\d/g;
+
+//     console.log(ans.match(reg));
+
+    let str = "My name is/ R2D2";
+
+    console.log(str.match(/\//i));
